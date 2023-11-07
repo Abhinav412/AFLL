3de15b7cded38aa929debc95d9456c4e88de2438
Abhinav <abhinavdasu0412@gmail.com>
2023-11-07 11:58:42 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,82 @@
+import ply.lex as lex
+
+reserved = {
+    'if' : 'IF',
+    'else' : 'ELSE'
+}
+tokens = ('ID', 'EQ', 'INT', 'COLON', 'INDENT', 'DEDENT','ASSIGN')+tuple(reserved.values())
+
+t_IF = r'if'
+t_ELSE = r'else'
+t_EQ = r'=='
+t_COLON = r':'
+t_ASSIGN = r'='
+t_INDENT = r'\n[ \t]+'
+t_DEDENT = r'\n[ \t]*'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+def t_INT(t):
+    r'\d+'
+    return t
+
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+def lex_analyse(text):
+    lexer.input(text)
+
+    while True:
+        token = lexer.token()
+        if not token:
+            break
+        print(token)
+
+text=input()
+lex_analyse(text)
+
+import ply.yacc as yacc
+
+def p_statement_if(p):
+    'statement : IF ID EQ INT COLON ID ASSIGN INT'
+    p[0] = ('if', p[2], p[4], p[6], p[8])
+
+def p_statement_if_else(p):
+    'statement : IF ID EQ INT COLON ID ASSIGN INT ELSE COLON ID ASSIGN INT'
+    p[0] = ('if-else', p[2], p[4], p[6], p[8], p[11], p[1])
+
+syntax_error = False
+
+def p_error(p):
+    global syntax_error
+    syntax_error = True
+    if p:
+        print(f"Syntax error at '{p.value}'")
+    else:
+        print("Syntax error at EOF")
+
+parser = yacc.yacc()
+
+def parse(input):
+    global syntax_error
+    syntax_error = False
+    result = parser.parse(input, lexer=lexer)
+    if syntax_error:
+        print("Incorrect Syntax")
+    else:
+        print("Correct Syntax")
+    print(result)
+
+parse(text)
